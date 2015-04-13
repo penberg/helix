028f1bb3b04029de38239393d09005ed8e69fecb
Pekka Enberg <penberg@iki.fi>
2015-04-13 20:23:53 +0300

    tools/helix-trace: CSV output format support
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- tools/helix-trace/helix-trace.c
+++ tools/helix-trace/helix-trace.c
@@ -16,7 +16,14 @@ struct config {
 	const char *multicast_proto;
 	const char *multicast_addr;
 	int multicast_port;
+	const char *format;
 	const char *output;
+};
+
+struct trace_fmt_ops {
+	void (*fmt_header)(void);
+	void (*fmt_ob)(helix_order_book_t ob);
+	void (*fmt_trade)(helix_trade_t trade);
 };
 
 static uv_buf_t alloc_packet(uv_handle_t* handle, size_t suggested_size)
@@ -26,7 +33,11 @@ static uv_buf_t alloc_packet(uv_handle_t* handle, size_t suggested_size)
 	return uv_buf_init(rx_buffer, sizeof(rx_buffer));
 }
 
-static void print_top(helix_order_book_t ob)
+static void fmt_pretty_header(void)
+{
+}
+
+static void fmt_pretty_ob(helix_order_book_t ob)
 {
 	uint64_t timestamp = helix_order_book_timestamp(ob);
 	uint64_t timestamp_in_sec = timestamp / 1000;
@@ -46,14 +57,58 @@ static void print_top(helix_order_book_t ob)
 	}
 }
 
+static void fmt_pretty_trade(helix_trade_t trade)
+{
+	fprintf(output, "%s | %.3f |\n", helix_trade_symbol(trade), helix_trade_price(trade)/10000.0);
+}
+
+struct trace_fmt_ops fmt_pretty_ops = {
+	.fmt_header	= fmt_pretty_header,
+	.fmt_ob		= fmt_pretty_ob,
+	.fmt_trade	= fmt_pretty_trade,
+};
+
+static void fmt_csv_header(void)
+{
+	fprintf(output, "Symbol,Timestamp,BidPrice,BidSize,AskPrice,AskSize,LastPrice\n");
+}
+
+static void fmt_csv_ob(helix_order_book_t ob)
+{
+	if (helix_order_book_state(ob) == HELIX_TRADING_STATE_TRADING) {
+		fprintf(output, "%s,%lu,%f,%lu,%f,%lu,\n",
+			helix_order_book_symbol(ob),
+			helix_order_book_timestamp(ob),
+			(double)helix_order_book_bid_price(ob, 0)/10000.0,
+			helix_order_book_bid_size(ob, 0),
+			(double)helix_order_book_ask_price(ob, 0)/10000.0,
+			helix_order_book_ask_size(ob, 0)
+			);
+	}
+}
+
+static void fmt_csv_trade(helix_trade_t trade)
+{
+	fprintf(output, "%s,%lu,,,,,%f\n",
+		helix_trade_symbol(trade), helix_trade_timestamp(trade), helix_trade_price(trade)/10000.0);
+}
+
+struct trace_fmt_ops fmt_csv_ops = {
+	.fmt_header	= fmt_csv_header,
+	.fmt_ob		= fmt_csv_ob,
+	.fmt_trade	= fmt_csv_trade,
+};
+
+struct trace_fmt_ops *fmt_ops;
+
 static void process_ob_event(helix_order_book_t ob)
 {
-	print_top(ob);
+	fmt_ops->fmt_ob(ob);
 }
 
 static void process_trade_event(helix_trade_t trade)
 {
-	fprintf(output, "%s | %.3f |\n", helix_trade_symbol(trade), helix_trade_price(trade)/10000.0);
+	fmt_ops->fmt_trade(trade);
 }
 
 static void recv_packet(uv_udp_t* handle, ssize_t nread, uv_buf_t buf, struct sockaddr* addr, unsigned flags)
@@ -82,6 +137,7 @@ static void usage(void)
 		"    -c, --multicast-proto proto  UDP multicast protocol listen to.\n"
 		"    -a, --multicast-addr addr    UDP multicast address to listen to.\n"
 		"    -p, --multicast-port port    UDP multicast port to listen to.\n"
+		"    -f, --format format          Output format (pretty, csv).\n"
 		"    -h, --help                   display this help and exit\n",
 		program);
 	exit(1);
@@ -92,17 +148,20 @@ static struct option trace_options[] = {
 	{"multicast-proto", required_argument, 0, 'c'},
 	{"multicast-addr",  required_argument, 0, 'a'},
 	{"multicast-port",  required_argument, 0, 'p'},
+	{"format",          required_argument, 0, 'f'},
 	{"help",            no_argument,       0, 'h'},
 	{0, 0, 0, 0}
 };
 
 static void parse_options(struct config *cfg, int argc, char *argv[])
 {
+	cfg->format = "pretty";
+
 	for (;;) {
 		int opt_idx = 0;
 		int c;
 
-		c = getopt_long(argc, argv, "s:c:a:o:p:h", trace_options, &opt_idx);
+		c = getopt_long(argc, argv, "s:c:a:o:p:f:h", trace_options, &opt_idx);
 		if (c == -1)
 			break;
 
@@ -121,6 +180,9 @@ static void parse_options(struct config *cfg, int argc, char *argv[])
 			break;
 		case 'p':
 			cfg->multicast_port = strtol(optarg, NULL, 10);
+			break;
+		case 'f':
+			cfg->format = optarg;
 			break;
 		case 'h':
 			usage();
@@ -160,6 +222,15 @@ int main(int argc, char *argv[])
 
 	if (!cfg.multicast_port) {
 		fprintf(stderr, "error: multicast port is not specified. Use the '-p' option to specify it.\n");
+		exit(1);
+	}
+
+	if (!strcmp(cfg.format, "pretty")) {
+		fmt_ops = &fmt_pretty_ops;
+	} else if (!strcmp(cfg.format, "csv")) {
+		fmt_ops = &fmt_csv_ops;
+	} else {
+		fprintf(stderr, "error: %s: unsupported format\n", cfg.format);
 		exit(1);
 	}
 
@@ -207,6 +278,8 @@ int main(int argc, char *argv[])
 	if (err) {
 		libuv_error("uv_udp_recv_start");
 	}
+
+	fmt_ops->fmt_header();
 
 	uv_run(uv_default_loop(), UV_RUN_DEFAULT);
 }
