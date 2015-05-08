835a9e87181718f72e9ae1f94edd184daf5385ef
Pekka Enberg <penberg@iki.fi>
2015-05-08 22:28:00 +0300

    nasdaq/soupfile: Fix end of session handling
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- src/nasdaq/soupfile.cc
+++ src/nasdaq/soupfile.cc
@@ -15,7 +15,14 @@ soupfile_session::soupfile_session(shared_ptr<net::message_parser> parser)
 
 size_t soupfile_session::parse(const net::packet_view& packet)
 {
-    size_t nr = _parser->parse(packet);
+    size_t nr = 0;
+    const char ch = *packet.buf();
+    if (ch != 0x0d) {
+        nr = _parser->parse(packet);
+        if (!nr) {
+            return nr;
+        }
+    }
     size_t remaining = packet.len() - nr;
     static constexpr size_t terminator_size = 2;
     if (remaining < terminator_size) {
